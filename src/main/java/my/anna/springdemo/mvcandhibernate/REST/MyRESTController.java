a9fa7e334ec7155d34bec45f4442f9fc4d2adbe3
AnnaBurd <burdanovaas@gmail.com>
2022-07-15 09:00:36 +0700

    added delete REST api

--- src/main/java/my/anna/springdemo/mvcandhibernate/REST/MyRESTController.java
+++ src/main/java/my/anna/springdemo/mvcandhibernate/REST/MyRESTController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,6 +53,30 @@ public class MyRESTController {
 		empService.saveEmployee(emp);
 		// Hibernate automatically updates id after save to db
 		return emp;		
+	}
+	
+	@PutMapping("/employees")
+	public Emp updateEmployee(@RequestBody Emp emp) {
+		
+		empService.saveEmployee(emp);
+		return emp;		
+	}
+	
+	@DeleteMapping("/employees/{id}")
+	public String deleteEmp(@PathVariable int id) {
+		
+		Emp emp = empService.getEmployee(id);
+		if (emp == null) {
+			
+			throw new NoSuchEmpException("There is no emp with id = " + id);
+		}
+		
+		empService.deleteEmployee(id);
+		
+		
+		
+		return new String("EMp with id=" + id + " was deleted");
+		
 	}
 
 
